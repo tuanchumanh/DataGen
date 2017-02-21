0648adea21b2afdbb8907ed7df5a9dc34349a566
TuanCM91 <tuancm91@gmail.com>
2017-02-21 18:59:28 +0900

    length bug

--- DataGenerator/Generator.cs
+++ DataGenerator/Generator.cs
@@ -66,7 +66,7 @@ namespace DataGenerator
 			}
 
 			columnName = GetAbbreviatedName(columnName);
-			if (columnName.Length >= length + 1)
+			if (columnName.Length >= length)
 			{
 				return columnName[0] + numberDict.AddOrUpdate(columnName, 1, (key, oldValue) => oldValue + 1).ToString().PadLeft(length - 1, '0');
 			}
